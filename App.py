906cda279d87c1e76dfc80206459d32b175ebaaa
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-04-26 23:37:38 -0500

    Updating intro page

--- App.py
+++ App.py
@@ -9,4 +9,6 @@ Use the navigation menu on the left to switch between:
 - Percentage Maps 
 - Charts  
 - Statistical Summaries  
+         
+My name is Lauren Hunt, and I am an urban planning professional creating this app as a capstone project for my Master of Sustainable Urban Development degree and DePaul University. The data visualized in this app was created by the Chicago Metropolitan Agency for Planning (CMAP). I recently completed a GIS internship at CMAP and was inspired to explore the data I helped to create further through this app.
 """)

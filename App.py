84903889335e0c25b5e0bc893557b8a49ad64f13
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-04-26 23:43:28 -0500

    Updating intro page

--- App.py
+++ App.py
@@ -10,5 +10,5 @@ Use the navigation menu on the left to switch between:
 - Charts  
 - Statistical Summaries  
          
-My name is Lauren Hunt, and I am anurban planning professional. I created this app as my capstone project for my Master's of Sustainable Urban Development at DePaul University. The data visualized in this app are the Land Use Inventory Datasets (1990, 2001, 2005, 2010, 2015, 2020, 2023) created by the Chicago Metropolitan Agency for Planning (CMAP). I was a GIS intern at CMAP in summer 2025 and was inspired to explore the data I helped to create further through this app.
+My name is Lauren Hunt, and I am an urban planning professional. I created this app as my capstone project for my Master's of Sustainable Urban Development at DePaul University. The data visualized in this app are the Land Use Inventory Datasets (1990, 2001, 2005, 2010, 2015, 2020, 2023) created by the Chicago Metropolitan Agency for Planning (CMAP). I was a GIS intern at CMAP in summer 2025 and was inspired to explore the data I helped to create further through this app.
 """)
